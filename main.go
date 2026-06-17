5997ec6c9123ed031cc6263a230c7f0c448f2c04
mmicbee <ann608888@gmail.com>
2026-06-18 02:04:37 +0300

    fixed a typo error

--- main.go
+++ main.go
@@ -5,10 +5,7 @@ import "fmt"
 func main () {
 	word :="you are a prude"
 
-	fmt.Println("you are a prude")
-(Capitalize(word))
-	fmt.Println("you are a prude")
-(ToUpper(word))
-	fmt.Println("you are a prude")
-(ToLower("DUMBASS"))
+	fmt.Println(Capitalize(word))
+	fmt.Println(ToUpper(word))
+	fmt.Println(ToLower("DUMBASS"))
 }
